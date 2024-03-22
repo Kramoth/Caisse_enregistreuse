c3c612b8884bea05fbfbb94cb302d5141f2b0731
kramoth <kramoth@udm.ac.mu>
2024-03-22 09:06:27 +0400

    translating french variable to english

--- script/caisse.c
+++ script/caisse.c
@@ -7,30 +7,30 @@ double alea(int min, int max){
 	return (double)rand()/(RAND_MAX)*(max-min)+min;
 }
 
-double calcule_monnaie(double montant_paye,double montant_du){
-	return montant_paye-montant_du;
+double calcule_monnaie(double paid_amount,double montant_du){
+	return paid_amount-montant_du;
 
 }
 
-void displayReturnedMoney(int* billet_a_rendre,int* billets, int* piece_a_rendre,double* pieces){
+void displayReturnedMoney(int* note_to_return,int* notes, int* coin_to_return,double* coins){
 	for(int i=0;i<7;i++)
 	{
-		if (billet_a_rendre[i]>0)
-				printf("we need %d note(s) of Rs%d\n",billet_a_rendre[i],billets[i]);
+		if (note_to_return[i]>0)
+				printf("we need %d note(s) of Rs%d\n",note_to_return[i],notes[i]);
 	}
 	for(int i=0;i<7;i++)
 	{
-		if (piece_a_rendre[i]>0)
-				printf("we need %d coin(s) of Rs%.2f\n",piece_a_rendre[i],pieces[i]);
+		if (coin_to_return[i]>0)
+				printf("we need %d coin(s) of Rs%.2f\n",coin_to_return[i],coins[i]);
 	}
 }
-void displayStock(int* stock_billets, int*billets, int* stock_pieces, double* pieces )
+void displayStock(int* stock_notes, int*notes, int* stock_coins, double* coins )
 {
 	printf("-------------Stock-----------\n");
 	for(int i=0;i<7;i++)
-		printf("Rs%d : %d\n",billets[i],stock_billets[i]);
+		printf("Rs%d : %d\n",notes[i],stock_notes[i]);
 	for(int i=0;i<7;i++)
-		printf("Rs%.2f : %d\n",pieces[i],stock_pieces[i]);
+		printf("Rs%.2f : %d\n",coins[i],stock_coins[i]);
 	printf("-----------------------------\n");
 }
 int checkStock(int* stock)
@@ -46,74 +46,74 @@ int checkStock(int* stock)
 }
 
 
-void refillStock(int* stock_billets,int* stock_pieces){
+void refillStock(int* stock_notes,int* stock_coins){
 	for(int i=0;i<7;i++)
 	{
-		stock_pieces[i]=10;
-		stock_billets[i]=10;
+		stock_coins[i]=10;
+		stock_notes[i]=10;
 	}
 }
 int main()
 {
-	int billets[7]={2000,1000,500,200,100,50,25};
-	double pieces[7]={20,10,5,1,0.5,0.2,0.05};
-	int billet_a_rendre[7]={0,0,0,0,0,0,0};
-	int piece_a_rendre[7]={0,0,0,0,0,0,0};
-	int stock_billets[7]={2,10,10,20,20,30,40};
-	int stock_pieces[7]={10,20,20,40,10,10,10};
-	double montant_paye;
+	int notes[7]={2000,1000,500,200,100,50,25};
+	double coins[7]={20,10,5,1,0.5,0.2,0.05};
+	int note_to_return[7]={0,0,0,0,0,0,0};
+	int coin_to_return[7]={0,0,0,0,0,0,0};
+	int stock_notes[7]={2,10,10,20,20,30,40};
+	int stock_coins[7]={10,20,20,40,10,10,10};
+	double paid_amount;
 	srand((unsigned int)time(NULL));
 	int next=1;
 	while(next)
 	{
-		double a_payer=round(alea(0,10000)*100)/100;
+		double amount_to_pay=round(alea(0,10000)*100)/100;
 
-		printf("You have to pay: Rs%.2f\n",a_payer);
+		printf("You have to pay: Rs%.2f\n",amount_to_pay);
 
 		printf("Please insert money: ");
-		scanf("%lf",&montant_paye);
-		while(montant_paye<a_payer){
+		scanf("%lf",&paid_amount);
+		while(paid_amount<amount_to_pay){
 			printf("Insufficient fund, please insert money: ");
-			scanf("%lf",&montant_paye);
+			scanf("%lf",&paid_amount);
 		}
-		printf("You have inserted Rs.%.2f\n",montant_paye);
-		double a_rendre=calcule_monnaie(montant_paye,a_payer);
-		printf("I will return back Rs%.2f\n",a_rendre);
-		int no_billet=0;
-		int no_piece=0;
-		while(a_rendre>25 && !no_billet)
+		printf("You have inserted Rs.%.2f\n",paid_amount);
+		double amount_to_return=calcule_monnaie(paid_amount,amount_to_pay);
+		printf("I will return back Rs%.2f\n",amount_to_return);
+		int no_note=0;
+		int no_coin=0;
+		while(amount_to_return>25 && !no_note)
 		{
 			for(int i=0;i<7;i++)
 			{
-				if(a_rendre-billets[i]>0)
+				if(amount_to_return-notes[i]>0)
 				{
-					if(stock_billets[i]>0){
-						a_rendre-=billets[i];
-						stock_billets[i]-=1;
-						billet_a_rendre[i]+=1;
+					if(stock_notes[i]>0){
+						amount_to_return-=notes[i];
+						stock_notes[i]-=1;
+						note_to_return[i]+=1;
 						break;	
 					}
 				}
 			}
-			no_billet=checkStock(stock_billets);
+			no_note=checkStock(stock_notes);
 		}
-		while(a_rendre>0.05 && !no_piece)
+		while(amount_to_return>0.05 && !no_coin)
 		{
 			for(int i=0;i<7;i++)
 			{
-				if(a_rendre-pieces[i]>0)
+				if(amount_to_return-coins[i]>0)
 				{
-					if(stock_pieces[i]>0){
-						a_rendre-=pieces[i];
-						stock_pieces[i]-=1;
-						piece_a_rendre[i]+=1;
+					if(stock_coins[i]>0){
+						amount_to_return-=coins[i];
+						stock_coins[i]-=1;
+						coin_to_return[i]+=1;
 						break;
 					}
 				}
 			}
-			no_piece=checkStock(stock_pieces);
+			no_coin=checkStock(stock_coins);
 		}
-		if(no_piece && no_billet)
+		if(no_coin && no_note)
 		{
 			int option;
 			printf("there is no more money\n");
@@ -121,12 +121,12 @@ int main()
 			printf("2- quit\n");
 			scanf("%d",&option);
 			if(option==1)
-				refillStock(stock_billets,stock_pieces);
+				refillStock(stock_notes,stock_coins);
 			else
-				exit(0);
+				next=0;
 
 		}
-		displayReturnedMoney(billet_a_rendre,billets,piece_a_rendre,pieces);
-		displayStock(stock_billets,billets,stock_pieces,pieces);
+		displayReturnedMoney(note_to_return,notes,coin_to_return,coins);
+		displayStock(stock_notes,notes,stock_coins,coins);
 	}
 }
